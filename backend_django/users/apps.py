4bb45cd28c295b7bab4bf62d7de7c8f5f00dd550
Nahi1221 <nahomm2010@gmail.com>
2026-09-04 16:54:32 +0300

    feat: auto-create superuser via post_migrate signal (no shell needed)

--- backend_django/users/apps.py
+++ backend_django/users/apps.py
@@ -1,5 +1,33 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+import os
+
+
+def create_superuser(sender, **kwargs):
+    if sender.name != 'users':
+        return
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if User.objects.filter(is_superuser=True).exists():
+        return
+    email = os.environ.get('ADMIN_EMAIL', 'admin@kitchenos.app')
+    password = os.environ.get('ADMIN_PASSWORD', 'Admin123')
+    if not email or not password:
+        return
+    User.objects.create_superuser(
+        username=email,
+        email=email,
+        password=password,
+        name='Admin User',
+        phone='0000000000',
+        business_name='KitchenOS Admin',
+        business_location='Headquarters',
+        user_type='admin',
+    )
 
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    def ready(self):
+        post_migrate.connect(create_superuser, sender=self)
